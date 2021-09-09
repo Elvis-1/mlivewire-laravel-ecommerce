de3cd13a1f511527d4939ec8ff04b71b65765944
Elvis <66243414+Elvis-1@users.noreply.github.com>
2021-09-09 14:44:03 +1300

    new ecommerce web with livewire

---
+++ resources/views/mylearn.php
@@ -0,0 +1,13 @@
+ <?php
+ /* Steps 
+ 1. start the project
+ 2. add install livewire -- composer require livewire/livewire
+ 3. Add add livewireStyles and livewireScripts to the base or app layout
+ 4 add homecomponent -- php artisan make:livewire HomeComponent.
+ 5. Attach base layout to the HomeComponent
+ 6. Add the HomeComponent to your route file(web.php)
+ 7. add content to your home-component view in livewire
+ 8. connect your images using asset
+
+
+ */
